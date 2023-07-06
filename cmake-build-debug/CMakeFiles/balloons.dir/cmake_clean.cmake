c7782bf55b796355b994b003bac13de5f75d3663
maxxboykin <maxericboykin@gmail.com>
2023-07-06 19:58:04 -0400

    refactoring with friend

---
+++ cmake-build-debug/CMakeFiles/balloons.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/balloons.dir/main.c.o"
+  "CMakeFiles/balloons.dir/main.c.o.d"
+  "balloons"
+  "balloons.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/balloons.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- structures/structures.h
+++ structures/structures.h
@@ -2,52 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 char *str_dup(char *s);
-static struct nlist *hashtable[HASH_SIZE];
-struct nlist {
-    int key;
-    struct nlist *next;
-    char *first;
-    char *last;
-    int *month;
-    int *day;
-    int *year;
-};
+static struct friend *hashtable[HASH_SIZE];
+//struct nlist {
+//    int key;
+//    struct nlist *next;
+//    char *first;
+//    char *last;
+//    int *month;
+//    int *day;
+//    int *year;
+//};
 
-typedef struct {
+struct friend {
+    int key;
+    struct friend *next;
     char *first;
     char *last;
-    int *month;
-    int *day;
-    int *year;
-} friend;
+    int month;
+    int day;
+    int year;
+};
 
 
 void print_hashtable(){
     for(unsigned i = 0; i < HASH_SIZE - 1; i++){
-        struct nlist *p = hashtable[i];
-        if(p != NULL) printf("%s %s\n", p->first, p->last);
-        while(p != NULL && p->next != NULL){
-            p = p->next;
-            printf("%s %s\n", p->first, p->last);
+        struct friend *f = hashtable[i];
+        // calculate age of friend here and place in print below
+        if(f != NULL) printf("%s %s %d %d %d\n", f->first, f->last, f->month, f->day, f->year);
+        while(f != NULL && f->next != NULL){
+            f = f->next;
+            printf("%s %s %d %d %d\n", f->first, f->last, f->month, f->day, f->year);
         }
     }
 }
 
-struct nlist *lookup(char *s){
-    struct nlist *np;
-    for(np = hashtable[hash(s)]; np != NULL; np = np->next){
-
+/*hash: form hash value for string s */
+unsigned hash(char *s){
+    unsigned hashval;
+    for(hashval = 0; *s != '\0'; s++){
+        hashval = *s + 31 * hashval;
     }
+    return hashval % HASH_SIZE;
 }
 
-struct nlist *install(friend friend){
-    struct nlist *np;
+struct friend *lookup(char *first){
+    struct friend *np;
+    for(np = hashtable[hash(first)]; np != NULL; np = np->next){
+        // there can be collisions for different names
+        // this is why you need to look through
+        if (strcmp(first, np->first) == 0){
+            return np; /* found */
+        }
+    }
+    return NULL;
+}
+
+struct friend *install(struct friend friend){
+    struct friend *np;
     unsigned hashval;
-    if((np = lookup(first)) == NULL) /* not found */ {
-        np = (struct nlist *) malloc(sizeof(*np));
-        if(np == NULL || (np->name = str_dup(name)) == NULL){
+    if((np = lookup(friend.first)) == NULL) /* not found */ {
+        np = (struct friend *) malloc(sizeof(*np));
+        if(np == NULL || (np->first = str_dup(friend.first)) == NULL){
             return NULL;
         }
+        hashval = hash(friend.first);
+
     }
 }
 
